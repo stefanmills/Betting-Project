6fb9bd37068a83566431ea66f225bf00df878fca
stefanmills <stefanmills8@gmail.com>
2021-04-09 01:16:23 +0100

    first commit

---
+++ sample.py
@@ -0,0 +1,34 @@
+#Full Time Result (H=Home Win, D=Draw, A=Away Win)
+#HTGD - Home team goal difference
+#ATGD - away team goal difference
+#HTP - Home team points
+#ATP - Away team points
+#DiffFormPts Diff in points
+#DiffLP - Differnece in last years prediction
+
+#Input - 12 other features (fouls, shots, goals, misses,corners, red card, yellow cards)
+#Output - Full Time Result (H=Home Win, D=Draw, A=Away Win)
+
+
+
+import pandas as pd
+import xgboost as xbg
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from IPython.display import display #for displayin result
+
+data=pd.read_csv('2019-20.csv')
+
+#data exploration
+#win rate for home team?
+
+matches=data.shape[0]
+features=data.shape[1]-1 #minus 1 because one will be the feature used (FTR)
+homewins=len(data[data.FTR=='H'])
+homeWinRate=float(homewins/matches *100)
+
+# print('Total num of matches : {}'.format(matches))
+# print('Number of features : {}'.format(features))
+# print('Number of home wins: {}'.format(homewins))
+# print('The home win rate : {:.2f}%'.format(homeWinRate))
+
